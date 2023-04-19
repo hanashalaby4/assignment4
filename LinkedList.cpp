aebc3fbf5d72d93f204f7a7b55a6cde6c8a970c2
hanashalaby4 <shalaby04@aucegypt.edu>
2023-04-20 02:11:05 +0400

    main output correct

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -63,32 +63,55 @@ void LinkedList::remove(int v) { //removes a node given a its value
 
 void LinkedList::print() const { //prints the nodes
 	Node* current = head;
+	cout << "The modified linked list is: "<<endl;
 	while (current) {
-		cout << current->value<< " -> ";
+		cout << current->value<< " occurs "<<current->occurences<<" times ";
 		current = current->next;
+		cout << endl;
 	}
-
-
-	
+	cout << endl;
 }
+
+
 
 int LinkedList::sum() {
 	int sum = 0;
 	Node* current = head;
 	while (current) {
-		sum += current->value; //adds the value of the node multiplied by the number of times it is repeated to the total
+		sum += current->value*current->occurences; //adds the value of the node multiplied by the number of times it is repeated to the total
 		current = current->next;
 	}
 	return sum;
 }
 
-LinkedList LinkedList::fromVector(const std::vector<int>& v) { //static so the function can be used without
-	LinkedList list;                                      //having objects of the class LinkedList, given the function recieves a vector
-	for (vector<int>::const_iterator it = v.begin(); it != v.end(); ++it) { //iteration through vector
-		int value = *it;
-		int o = count(v.begin(), v.end(), value); //counts the value's repetitions in the vector from v.begin to v.end
-												  // and stores it in the variable o
-		list.add(value, o); //adds a node to the linked list with the value and its occurences
+LinkedList LinkedList::fromVector(const std::vector<int>& v) {
+	LinkedList list;
+
+	for (int i = 0; i < v.size(); ++i) {
+		int value = v[i];
+
+		// Check if the value has not been added to the list already
+		bool alreadyAdded = false;
+		Node* current = list.head;
+		while (current) {
+			if (current->value == value) {
+				alreadyAdded = true;
+				break;
+			}
+			current = current->next;
+		}
+
+		if (!alreadyAdded) { //skip if the value is already in the vector
+			int o = count(v.begin(), v.end(), value);
+			                                          //counts the value's repetitions in the vector from v.begin to v.end
+			list.add(value, o);                       // and stores it in the variable o
+		}
 	}
-	return list; //returns the list
+
+	return list;
 }
+ 
+
+
+
+
